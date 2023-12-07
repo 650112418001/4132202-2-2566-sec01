edd9d9ab912518617235f07296d1b2a377fc05bb
650112418001 <650112418001@bru.ac.th>
2023-12-07 15:54:39 +0700

    LAB3 PAST2

--- LAB3/html/listitem.php
+++ LAB3/html/listitem.php
@@ -4,12 +4,12 @@ include "condb.php";
 $sql = "SELECT * FROM tb_member";
 $result = mysqli_query($conn, $sql);
 
-//var_dump($result);
+// var_dump($result);
 ?>
 
-<button id="btn_add"> + Add </button>
+<button id="btn_add" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop"> + Add </button>
 
-<table>
+<table class="table table-hover">
     <thead>
         <tr>
             <th>ID</th>
@@ -27,7 +27,8 @@ $result = mysqli_query($conn, $sql);
                 <td><?= $row["id_member"] ?></td>
                 <td><?= $row["name"] ?></td>
                 <td><?= $row["id_province"] ?></td>
-                <td><button class="btn_del" data-id="<?= $row["id_member"] ?>"> DEL </button></td>
+                <td><button class="btn_del btn btn-danger" data-id="<?= $row["id_member"] ?>"> DEL </button></td>
+                <td><button class="btn_edt" data-id="<?= $row["id_member"] ?>" data-bs-toggle="modal" data-bs-target="#staticBackdrop"> EDIT </button></td>
             </tr>
         <?php
         }
@@ -38,26 +39,39 @@ $result = mysqli_query($conn, $sql);
 <script>
     $(".btn_del").click(function() {
         let id = $(this).data("id");
-        console.log(id);    
+        console.log(id);
 
         $.ajax({
-            url:"/delitem.php",
-            method:"GET",
-            data:{
+            url: "/delitem.php",
+            method: "GET",
+            data: {
                 id_mem: id
             },
             success: function(res) {
-                console.log(res);   
-                if(res == "Error")
+                console.log(res);
+                if (res == "Error")
                     alert("Can't delete item.");
                 else
                     $("#div_item").load("/listitem.php");
+
             }
         });
     });
 
+
     $("#btn_add").click(function(){
-        $("#div_item").load("/addform.php");
+       // $("#div_item").load("/addform.php");
+       $("#staticBackdropLabel").text("Add Item");
+       $(".modal-body").load("/addform.php");
+       $(".modal-footer").hide();
+    });
+
+    $(".btn_edt").click(function() {
+        let id = $(this).data("id");
+        console.log(id);
+        $("#staticBackdropLabel").text("Edit Item");
+        $(".modal-body").load(`/editform.php?id=${id}`);
+        $(".modal-footer").hide();
     });
 
 
